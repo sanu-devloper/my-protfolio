4d20914705556197cab9c1ac7afa453eec269ca6
sanu-devloper <161156359+sanu-devloper@users.noreply.github.com>
2024-03-10 18:09:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></src=>
+ src="https://kit.fontawesome.com/58a810656e.js" crossorigin="anonymous"></>
+  var typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "UI-UX Designer",
+      "Backend Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+
+  
+    var typeData = new Typed(".role", {
+      strings: [
+        "Full Stack Developer",
+        "Web Developer",
+        "UI-UX Designer",
+        "Backend Developer",
+        "Coder",
+      ],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1000,
+    });
